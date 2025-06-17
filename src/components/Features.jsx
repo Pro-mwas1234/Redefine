a0407623b69c7ba7b28285be3b08bf79d14bc725
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2025-06-17 13:16:18 +0300

    Update Features.jsx
    slight update

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -123,7 +123,7 @@ const Features = () => (
         />
       </BentoTilt>
 
-      <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
+      <div className="grid h-[235vh] w-full grid-cols-2 grid-rows-3 gap-7">
         <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
             src="videos/feature-2.mp4"
@@ -163,24 +163,39 @@ const Features = () => (
           />
         </BentoTilt>
 
+        <BentoTilt className="bento-tilt_2 me-14 md:col-span-1 md:me-0">
+          <BentoCard
+            src="videos/c.mp4"
+            title={
+              <>
+                c<b>yb</b>er<b>punk</b>
+              </>
+            }
+            description="My Favorite cyberspace, futuristic anime"
+            isComingSoon
+          />
+        </BentoTilt>
+        <BentoTilt className="bento-tilt_2 me-15 md:col-span-1 md:me-0">
+          <BentoCard
+            src="videos/3.mp4"
+            title={
+              <>
+                c<b>yb</b>er<b>p<b>space</b></b>
+              </>
+            }
+            description="My Favorite cyberspace, futuristic anime"
+            isComingSoon
+          />
+        </BentoTilt>
+
         <BentoTilt className="bento-tilt_2">
-          <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
-            <h1 className="bento-title special-font max-w-64 text-black">
+          <div className="flex size-full flex-col justify-between bg-violet-300 p-2">
+            <h1 className="bento-title special-font max-w-80 text-black">
               M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
             </h1>
 
             <TiLocationArrow className="m-5 scale-[5] self-end" />
           </div>
-        </BentoTilt>
-
-        <BentoTilt className="bento-tilt_2">
-          <video
-            src="videos/feature-5.mp4"
-            loop
-            muted
-            autoPlay
-            className="size-full object-cover object-center"
-          />
         </BentoTilt>
       </div>
     </div>
